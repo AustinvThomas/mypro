1362934cac9ee00cf22dd973bc3f475dc9ab01c5
AustinvThomas <austincitesjam19@gmail.com>
2018-09-27 17:27:26 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi I am austin thomas</h1>
+    <p>I am doing my UG in kgisl institute of technology, Coimbatore</p>
+    <p>This would be my first ever experience of participating in an hackathon</p>
+    
+  </Layout>
+)
+
+export default IndexPage
